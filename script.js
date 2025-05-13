e0d7c7c0b9daea6cbd6492f9db6bba5dab36c0e3
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-05-13 22:51:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+window.addEventListener("DOMContentLoaded", () => {
+	const savedTheme = localStorage.getItem("theme") || 'light';
+	document.body.classList.add(`${savedTheme}-mode`);
+});
+
+document.getElementById("toggle-theme").addEventListener("click", ()=>{
+	if(document.body.classList.contains('dark-mode')){
+		document.body.classList.replace('dark-mode', 'light-mode');
+		localStorage.setItem('theme', 'ligth');
+	}else{
+		document.body.classList.replace('light-mode', 'dark-mode');
+		localStorage.setItem('theme', 'dark');
+	}
+});
